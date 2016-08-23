8c3f8fc5c91fd370d3b4fd1eb37939614a7a532d
sreenivas1591 <sreenivas1591@gmail.com>
2016-08-23 19:24:56 +0530

    Initial Commit

---
+++ sparkleformation/components/base.rb
@@ -0,0 +1,4 @@
+SparkleFormation.component(:base) do
+  AWSTemplateFormatVersion '2016-08-22'
+  description 'Sparkle Project Compute Template' 
+end
---
+++ sparkleformation/registry/instance_flavour.rb
@@ -0,0 +1,3 @@
+SfnRegistry.register(:instance_flavour) do
+  ['t2.micro', 't2.small'] 
+end
